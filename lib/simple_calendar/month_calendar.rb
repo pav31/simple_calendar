0f263777c268e76ee7246f492488d150a4e2bc5e
PavelG <vargan.pg@gmail.com>
2016-07-30 09:34:22 +0300

    Add number_of_months to month calendar

--- lib/simple_calendar/month_calendar.rb
+++ lib/simple_calendar/month_calendar.rb
@@ -2,9 +2,20 @@ module SimpleCalendar
   class MonthCalendar < SimpleCalendar::Calendar
     private
 
-      def date_range
-        (start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week).to_a
+    def date_range
+      months = []
+      number_of_months.times do |number|
+        current_month = (start_date + number.months)
+        starting = current_month.beginning_of_month.beginning_of_week
+        ending = current_month.end_of_month.end_of_week
+
+        months << (starting..ending).to_a
       end
+      months
+    end
+
+    def number_of_months
+      options.fetch(:number_of_months, 1)
+    end
   end
 end
-
